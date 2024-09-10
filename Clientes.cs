38c27ee2560c3c0c967ef817bdfd86885e786407
William Xavier <william.santos@aluno.unb.br>
2024-09-10 11:39:10 -0300

    classes clientes e pessoa física

---
+++ Clientes.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controle_Clientes_Inicial
+{
+    public class Clientes
+    {
+        public string nome {get; set; }
+
+        public string endereco {get; set; }
+
+        public float valor {get; set; }
+
+        public float valorImposto {get; set; }
+
+        public float total {get; set; }
+
+        public virtual void pagarImposto (float v)
+        {
+            valor = v;
+
+            valorImposto = valor * 10/100;
+
+            total = valor + valorImposto;
+        }
+    }
+}
